44fac077ea6aa7978945761fc20d2377abf804aa
Sean <gourmevel@gmail.com>
2025-12-28 11:21:47 +0900

    광고주 모집 글 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export function Footer() {
               variant="outline"
               size="lg"
               className="h-12 px-6 rounded-full border-white/20 hover:bg-white hover:text-black transition-all duration-300"
-              onClick={() => window.open('https://www.instagram.com/drinki.g/', '_blank')}
+              onClick={() => window.open('https://www.instagram.com/drinkig.official/', '_blank')}
             >
               <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3z" />
